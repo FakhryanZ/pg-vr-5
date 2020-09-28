52d8140234c2aa99b27f2df2b5aa992c95976939
FakhryanZ <fakhryanmuhammad@gmail.com>
2020-09-28 11:24:17 +0700

    finish tugas

--- Assets/Scripts/TakeObject.cs
+++ Assets/Scripts/TakeObject.cs
@@ -7,11 +7,13 @@ public class TakeObject : MonoBehaviour
     public GameObject bola, tangan;
     bool tertangkap = false;
     Vector3 posisiBola;
+    public Material myMaterial;
 
     // Start is called before the first frame update
     void Start()
     {
         posisiBola = bola.transform.position;
+        myMaterial.color = Color.red;
     }
 
     // Update is called once per frame
@@ -23,11 +25,13 @@ public class TakeObject : MonoBehaviour
             {
                 bola.transform.SetParent(tangan.transform);
                 bola.transform.localPosition = new Vector3(0f, -.672f, 0f);
+                myMaterial.color = Color.blue;
                 tertangkap = true;
             }else if (tertangkap)
             {
                 bola.transform.SetParent(null);
                 bola.transform.localPosition = posisiBola;
+                myMaterial.color = Color.red;
                 tertangkap = false;
             }
         }
